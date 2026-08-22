3e277663aa3d17e9affda1d5c3275d894c4aa8d6
athman-developer <mohammmathman7@gmail.com>
2026-08-22 16:47:46 +0300

    Use stable Java Camera2 build for TREWX Qiblah v3

--- qiblah_build/app/build.gradle.kts
+++ qiblah_build/app/build.gradle.kts
@@ -1,21 +1,21 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace = "com.qiblah.direction"
+    namespace = "com.trewx.qiblah"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.qiblah.direction"
-        minSdk = 23
+        applicationId = "com.trewx.qiblah"
+        minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "3.0"
     }
-}
 
-dependencies {
-    implementation("androidx.core:core-ktx:1.15.0")
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 }
