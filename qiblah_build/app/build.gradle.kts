a84385e2e052ae59f8974f8378a3a184835ccb6f
athman-developer <mohammmathman7@gmail.com>
2026-08-22 18:11:17 +0300

    Bump Qibla Compass version

--- qiblah_build/app/build.gradle.kts
+++ qiblah_build/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.trewx.qiblah"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "3.0"
+        versionCode = 4
+        versionName = "3.1"
     }
 
     compileOptions {
