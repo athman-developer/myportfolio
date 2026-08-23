2adc341bd60fd26e26c6ef3981196893e0437cce
athman-developer <mohammmathman7@gmail.com>
2026-08-23 12:34:17 +0300

    Bump Qibla Compass version for call-screen redesign

--- qiblah_build/app/build.gradle.kts
+++ qiblah_build/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.trewx.qiblah"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "4.0"
+        versionCode = 6
+        versionName = "4.1"
     }
 
     compileOptions {
