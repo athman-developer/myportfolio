b21db349304522d171a0a1893be14cf6d7ef392c
athman-developer <mohammmathman7@gmail.com>
2026-08-23 20:28:36 +0300

    Bump Qibla Compass to 4.4

--- qiblah_build/app/build.gradle.kts
+++ qiblah_build/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.trewx.qiblah"
         minSdk = 24
         targetSdk = 35
-        versionCode = 8
-        versionName = "4.3"
+        versionCode = 9
+        versionName = "4.4"
     }
 
     compileOptions {
